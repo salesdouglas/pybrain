be3dac46bed58ac1d9ff37fc7ed02bc8424fa4b8
Douglas Sales Amante <douglassales@gec.inatel.br>
2018-07-25 11:36:47 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="PyBrain",
+    version="0.3.3",
+    description="PyBrain is the Swiss army knife for neural networking.",
+    license="BSD",
+    keywords="Neural Networks Machine Learning",
+    url="http://pybrain.org",
+    packages=find_packages(exclude=['examples', 'docs']),
+    include_package_data=True,
+    test_suite='pybrain.tests.runtests.make_test_suite',
+    package_data={'pybrain': ['rl/environments/ode/models/*.xode']},
+    install_requires = ["scipy"],
+)
